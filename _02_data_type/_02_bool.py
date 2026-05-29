831042d0b1087346866829563fa3aa401c64305d
suhoo898 <suhoo1102@naver.com>
2026-05-29 17:49:34 +0900

    20260529_데이터타입 (number, bool)

---
+++ _02_data_type/_02_bool.py
@@ -0,0 +1,37 @@
+# 논리형 Boolean
+a = True
+b = False
+print(a, type(a))
+print(b, type(b))
+
+# 비교 연산
+# A > B : A가 B 보다 크면 True, 아니면 False
+# A >= B : A가 B 이상이면 True, 아니면 False
+# A < B
+# A <= B
+# A == B : A, B가 같으면 True, 아니면 False
+# A != B : A, B가 같지 않으면 True, 아니면 False
+
+print("1 > 0.5:", 1 > 0.5) # True
+print("1 < 0.5:", 1 < 0.5) # False
+print("1 >= 0.5:", 1 >= 0.5) # True
+print("1 <= 0.5:", 1 <= 0.5) # False
+print("1 == 1:", 1 == 1) # True
+print("1 != 1:", 1 != 1) # False
+
+# 논리 부정 연산(not)
+print(True)
+print(not True)
+print(not not True)
+
+
+
+
+
+
+
+
+
+
+
+
